44f5b5b1d5e569fdfe110acc3d644db32fad4d79
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2024-12-13 16:02:18 -0500

    Update some of the token definitions
    * Increase contrast for onContainer for muted and tonal containers (aligned with neutral container now).
    * Change the logic of containerOutline and containerOutlineVariant in dark mode for neutral, muted and tonal containers to use darker tonal stops instead of lighter ones.

--- chroma/src/main/java/org/pushingpixels/ephemeral/chroma/dynamiccolor/EphemeralChromaDynamicColors.java
+++ chroma/src/main/java/org/pushingpixels/ephemeral/chroma/dynamiccolor/EphemeralChromaDynamicColors.java
@@ -218,7 +218,7 @@ public final class EphemeralChromaDynamicColors {
     return new DynamicColor(
         /* name= */ "neutral_container_outline",
         /* palette= */ (s) -> s.mutedPalette,
-        /* tone= */ (s) -> s.isDark ? 60.0 : 50.0,
+        /* tone= */ (s) -> s.isDark ? 10.0 : 50.0,
         /* isBackground= */ false,
         /* background= */ this::highestSurface,
         /* secondBackground= */ null,
@@ -230,7 +230,7 @@ public final class EphemeralChromaDynamicColors {
     return new DynamicColor(
         /* name= */ "neutral_container_outline_variant",
         /* palette= */ (s) -> s.mutedPalette,
-        /* tone= */ (s) -> s.isDark ? 30.0 : 80.0,
+        /* tone= */ (s) -> s.isDark ? 20.0 : 80.0,
         /* isBackground= */ false,
         /* background= */ this::highestSurface,
         /* secondBackground= */ null,
@@ -465,7 +465,7 @@ public final class EphemeralChromaDynamicColors {
         /* palette= */ (s) -> s.mutedPalette,
         /* tone= */ (s) -> {
           if (isFidelity(s)) {
-            return DynamicColor.foregroundTone(mutedContainer().tone.apply(s), 4.5);
+            return DynamicColor.foregroundTone(mutedContainer().tone.apply(s), 6.0);
           }
           return s.isDark ? 90.0 : 30.0;
         },
@@ -492,7 +492,7 @@ public final class EphemeralChromaDynamicColors {
     return new DynamicColor(
         /* name= */ "muted_container_outline",
         /* palette= */ (s) -> s.mutedPalette,
-        /* tone= */ (s) -> s.isDark ? 60.0 : 50.0,
+        /* tone= */ (s) -> s.isDark ? 10.0 : 50.0,
         /* isBackground= */ false,
         /* background= */ this::highestSurface,
         /* secondBackground= */ null,
@@ -504,7 +504,7 @@ public final class EphemeralChromaDynamicColors {
     return new DynamicColor(
         /* name= */ "muted_container_outline_variant",
         /* palette= */ (s) -> s.mutedPalette,
-        /* tone= */ (s) -> s.isDark ? 40.0 : 70.0,
+        /* tone= */ (s) -> s.isDark ? 20.0 : 70.0,
         /* isBackground= */ false,
         /* background= */ this::highestSurface,
         /* secondBackground= */ null,
@@ -621,7 +621,7 @@ public final class EphemeralChromaDynamicColors {
         /* palette= */ (s) -> s.primaryPalette,
         /* tone= */ (s) -> {
           if (isFidelity(s)) {
-            return DynamicColor.foregroundTone(tonalContainer().tone.apply(s), 4.5);
+            return DynamicColor.foregroundTone(tonalContainer().tone.apply(s), 6.0);
           }
           return s.isDark ? 90.0 : 30.0;
         },
@@ -648,7 +648,7 @@ public final class EphemeralChromaDynamicColors {
     return new DynamicColor(
         /* name= */ "tonal_container_outline",
         /* palette= */ (s) -> s.primaryPalette,
-        /* tone= */ (s) -> s.isDark ? 60.0 : 45.0,
+        /* tone= */ (s) -> s.isDark ? 10.0 : 45.0,
         /* isBackground= */ false,
         /* background= */ this::highestSurface,
         /* secondBackground= */ null,
@@ -660,7 +660,7 @@ public final class EphemeralChromaDynamicColors {
     return new DynamicColor(
         /* name= */ "tonal_container_outline_variant",
         /* palette= */ (s) -> s.primaryPalette,
-        /* tone= */ (s) -> s.isDark ? 40.0 : 65.0,
+        /* tone= */ (s) -> s.isDark ? 20.0 : 65.0,
         /* isBackground= */ false,
         /* background= */ this::highestSurface,
         /* secondBackground= */ null,
