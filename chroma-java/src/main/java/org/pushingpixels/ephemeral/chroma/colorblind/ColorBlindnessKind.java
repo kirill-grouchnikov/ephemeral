f5de1377b444ab9aeb9700cd6cfeb5c768e1e52a
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-01-02 11:24:44 -0500

    Update copyright notice

--- chroma-java/src/main/java/org/pushingpixels/ephemeral/chroma/colorblind/ColorBlindnessKind.java
+++ chroma-java/src/main/java/org/pushingpixels/ephemeral/chroma/colorblind/ColorBlindnessKind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Ephemeral
+ * Copyright 2025-2026 Ephemeral
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
